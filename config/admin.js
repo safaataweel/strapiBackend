f8afd36ff960f0a0835a06e678483671655ef3ee
safaataweel <safaa.altaweel1223@gmail.com>
2024-08-12 12:31:42 +0300

    deployed changes12

--- config/admin.js
+++ config/admin.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
     secret: env('ADMIN_JWT_SECRET'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT','7D7vAkuvdfgzok7Gh7tuPg'),
   },
   transfer: {
     token: {
