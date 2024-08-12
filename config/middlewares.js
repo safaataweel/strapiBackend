3407e9ab950e15d17e8164c51a3a73093175dd12
safaataweel <safaa.altaweel1223@gmail.com>
2024-08-12 11:41:51 +0300

    deployed changes6

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,11 +1,12 @@
-
-// config/middlewares.js
-module.exports = {
-  settings: {
-    cors: {
-      origin: '*', // Or specify your frontend URL
-      methods: ['GET', 'POST', 'PUT', 'DELETE'],
-      headers: '*',
-    },
-  },
-};
+module.exports = [
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
