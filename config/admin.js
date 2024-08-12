e945dd64d5f389cfac19d89839a9cd8a930191c6
safaataweel <safaa.altaweel1223@gmail.com>
2024-08-12 12:56:00 +0300

    deployed changes15

--- config/admin.js
+++ config/admin.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET','04sDR74LubhXM3HOYKK0Gp3qmRyNgh8tF1a3y+Mqzvk='),
   },
   apiToken: {
     salt: process.env.API_TOKEN_SALT || 'vme6YdCxw1pk+I2NdxvF9A==',
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT','R1dPFeQMI+fDEZ2iHxL4j6refUZBLkWQomkN/8+BDmw='),
     },
   },
   flags: {
