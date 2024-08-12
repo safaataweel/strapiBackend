d3f9b40b125c617274e58cbf82fb7618bad598c5
safaataweel <safaa.altaweel1223@gmail.com>
2024-08-12 11:13:18 +0300

    deployed changes2

--- config/server.js
+++ config/server.js
@@ -2,7 +2,9 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', [
+      '9a0799921cc397336a4ea66ecf471f61237f73d841882103f3b656bb698ee66c9195186fa13f4c1168b267a9048311c9716c5dd253e97eb2545c4cdb2f51e176'
+    ]),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
